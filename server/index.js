eff25ef72a3b5a0fc2524bde5d41ada0b523cbe2
freddy <freddykaberuka2009@gmail.com>
2020-11-27 15:33:15 +0200

    add signup api

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import signupRoute from './routes/signup';
 const app=express();
 const port=process.env.port||3000;
 dotenv.config();
@@ -11,7 +12,8 @@ mongoose
      .then(() => console.log( 'Database Connected' ))
      .catch(err => console.log( err ));
 
-
+     app.use(express.json());
+     app.use("/api/v1/user", signupRoute);
 app.get('/home');
 app.listen(port, () => {
     console.log(`localhost:${port}`);
