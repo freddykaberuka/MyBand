a90deebfccb5178c76c540828ced99f37c812e00
freddy <freddykaberuka2009@gmail.com>
2020-11-27 17:03:48 +0200

    add contact-Restful Apis

---
+++ server/model/contact.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from 'mongoose';
+
+const contactsSchema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        match: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}/,
+        required: true
+    },
+    subject: {
+        type: String,
+        required: true
+    },
+    comment: {
+        type: String,
+        required: true
+    },
+
+});
+
+module.exports = mongoose.model('Contact', contactsSchema);
